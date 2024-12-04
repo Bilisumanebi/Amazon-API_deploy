ae9534bdd4ce7b37df3e81a1e5d7b760b2dc5880
Bilisumanebi <hbilisuma@yahoo.com>
2024-12-03 21:27:20 -0600

    Amazon BackEnd deployment

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+
+const express = require('express');
+const cors = require('cors');
+const  dotenv = require('dotenv');
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({origin: true}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+      res.status(200).json({message:'Hello World'});
+}     );
+
+app.post('/payments/create', async (req, res) => { 
+      const total = req.query.total;
+      
+      if (total > 0) {
+            const paymentIntent = await stripe.paymentIntents.create({
+                  amount: total,
+                  currency: 'usd',
+            });
+            console.log(paymentIntent);
+            
+            res.status(201).json({
+                  clientSecret: paymentIntent.client_secret,
+            });
+            }
+            else {
+                  res.status(403).json({message: 'Invalid Total Amount, total must be > 0'});
+            }
+      });
+
+
+app.listen(3000, (err)=> {
+      if (err) throw err;
+      console.log('Server is running on port: 3000, http://localhost:3000');
+})
+
+
